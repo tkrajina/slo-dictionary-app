cdf125d9ac42f8e7ce514ce0b4531660233affbc
Tomo Krajina <tkrajina@gmail.com>
2020-12-05 20:17:33 +0100

    Generic WordInfo

--- src/components/WordInfo.tsx
+++ src/components/WordInfo.tsx
@@ -2,7 +2,7 @@ import { default as React } from "react";
 import { Image, Text, View } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { LINK_24PX } from "../images_generated";
-import { AbstractWord } from "../models/models";
+import { AbstractWord, CollocationEntry, ThesaurusEntry } from "../models/models";
 import * as Utils from "../utils/utils";
 import { HorizontalLine } from "./HorizontalLine";
 import { Progress } from "./Progress";
@@ -34,7 +34,6 @@ export class WordInfo extends React.PureComponent<WordInfoProps /*, WordInfoStat
   }
 
   renderLong() {
-    const info = this.props.word.info();
     return (
       <React.Fragment>
         <View style={{ marginTop: 10 }}>
@@ -44,49 +43,18 @@ export class WordInfo extends React.PureComponent<WordInfoProps /*, WordInfoStat
         <View style={{ marginTop: 5 }}>
           <Text style={{ color: "#888", fontSize: 10 }}>Sopomenke:</Text>
           <HorizontalLine color="#ddd" />
-          {this.renderWords(info as [number, string][][][])}
+          {this.renderLongWords()}
         </View>
       </React.Fragment>
     );
   }
 
-  renderWords(wordsGroup: [number, string][][][]) {
-    if (!wordsGroup) {
-      return null;
+  renderLongWords() {
+    if (this.props.word instanceof ThesaurusEntry) {
+      return <LongThesaurus info={this.infoAsThesaurus()} callbackOnClickWord={this.callbackOnClickWord} />;
+    } else if (this.props.word instanceof CollocationEntry) {
     }
-    return (
-      <React.Fragment>
-        {wordsGroup.map((g1) => (
-          <React.Fragment>
-            {g1.map((g2) => (
-              <React.Fragment>{g2.map((word) => this.renderWord(word))}</React.Fragment>
-            ))}
-            {g1.length > 0 && <HorizontalLine color="#ddd" />}
-          </React.Fragment>
-        ))}
-      </React.Fragment>
-    );
-  }
-
-  renderWord(word: [number, string]) {
-    const score = Math.max(0, Math.min(0.99, word[0] / 0.4));
-    const colors = [/*'e', 'd', 'c',*/ "b", "a", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
-    const color = "#" + colors[Math.trunc(score * colors.length)].repeat(3);
-    return (
-      <TouchableOpacity onPress={() => this.callbackOnClickWord(word[1])}>
-        <View style={{ flexDirection: "row" }}>
-          <View style={{ width: 60, paddingTop: 8 }}>
-            <Progress width={55} height={12} percentage={score} color={color} />
-          </View>
-          <View style={{ flex: 1, padding: 2 }}>
-            <Text style={{ color: color, fontSize: 16 }}>{word[1]}</Text>
-          </View>
-          <View style={{ width: 30 }}>
-            <Image source={LINK_24PX} style={{ opacity: 0.25 }} />
-          </View>
-        </View>
-      </TouchableOpacity>
-    );
+    return <Text>Unknown: {JSON.stringify(this.props.word.info())}</Text>;
   }
 
   infoAsThesaurus() {
@@ -98,30 +66,12 @@ export class WordInfo extends React.PureComponent<WordInfoProps /*, WordInfoStat
   }
 
   renderShort() {
-    const info = this.infoAsThesaurus();
-    let words: string[] = [];
-    for (const g1 of info) {
-      for (const g2 of g1) {
-        for (const word of g2) {
-          words.push(word[1]);
-        }
-      }
-    }
-    if (words.length > 5) {
-      words = words.slice(0, 5);
-    }
-
     return (
       <TouchableOpacity onPress={() => this.callbackOnClickWord(this.props.word.word)}>
         <View style={{ flexDirection: "row" }}>
           <View style={{ flex: 1 }}>
             <Text style={{ fontSize: 20 }}>{this.renderWithHighlight(this.props.word.word)}:</Text>
-            <Text style={{ paddingLeft: 10, fontStyle: "italic" }}>
-              {words.map((w, _index) => (
-                <Text key={`${w}`}>{w}, </Text>
-              ))}{" "}
-              ...
-            </Text>
+            <Text style={{ paddingLeft: 10, fontStyle: "italic" }}>{this.renderShortWords()}</Text>
           </View>
           <View style={{ width: 30 }}>
             <Image source={LINK_24PX} style={{ opacity: 0.25 }} />
@@ -129,6 +79,14 @@ export class WordInfo extends React.PureComponent<WordInfoProps /*, WordInfoStat
         </View>
       </TouchableOpacity>
     );
+  }
+
+  renderShortWords() {
+    if (this.props.word instanceof ThesaurusEntry) {
+      return <ShortThesaurus info={this.infoAsThesaurus()} />;
+    } else if (this.props.word instanceof CollocationEntry) {
+    }
+    return <Text>Unknown: {JSON.stringify(this.props.word.info())}</Text>;
   }
 
   renderWithHighlight(text: string) {
@@ -162,5 +120,84 @@ export class WordInfo extends React.PureComponent<WordInfoProps /*, WordInfoStat
       }
     }
     return res;
+  }
+}
+
+interface LongThesaurusProps {
+  info: [number, string][][][];
+  callbackOnClickWord: (word: string) => void;
+}
+class LongThesaurus extends React.Component<LongThesaurusProps> {
+  constructor(props: LongThesaurusProps) {
+    super(props);
+  }
+  render() {
+    if (!this.props.info) {
+      return null;
+    }
+    return (
+      <React.Fragment>
+        {this.props.info.map((g1) => (
+          <React.Fragment>
+            {g1.map((g2) => (
+              <React.Fragment>{g2.map((word) => this.renderWord(word))}</React.Fragment>
+            ))}
+            {g1.length > 0 && <HorizontalLine color="#ddd" />}
+          </React.Fragment>
+        ))}
+      </React.Fragment>
+    );
+  }
+
+  renderWord(word: [number, string]) {
+    const score = Math.max(0, Math.min(0.99, word[0] / 0.4));
+    const colors = [/*'e', 'd', 'c',*/ "b", "a", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0"];
+    const color = "#" + colors[Math.trunc(score * colors.length)].repeat(3);
+    return (
+      <TouchableOpacity onPress={() => this.props.callbackOnClickWord(word[1])}>
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ width: 60, paddingTop: 8 }}>
+            <Progress width={55} height={12} percentage={score} color={color} />
+          </View>
+          <View style={{ flex: 1, padding: 2 }}>
+            <Text style={{ color: color, fontSize: 16 }}>{word[1]}</Text>
+          </View>
+          <View style={{ width: 30 }}>
+            <Image source={LINK_24PX} style={{ opacity: 0.25 }} />
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+interface ShortThesaurusProps {
+  info: [number, string][][][];
+}
+class ShortThesaurus extends React.Component<ShortThesaurusProps> {
+  constructor(props: ShortThesaurusProps) {
+    super(props);
+  }
+  render() {
+    let words: string[] = [];
+    for (const g1 of this.props.info) {
+      for (const g2 of g1) {
+        for (const word of g2) {
+          words.push(word[1]);
+        }
+      }
+    }
+    if (words.length > 5) {
+      words = words.slice(0, 5);
+    }
+
+    return (
+      <React.Fragment>
+        {words.map((w, _index) => (
+          <Text key={`${w}`}>{w}, </Text>
+        ))}{" "}
+        ...
+      </React.Fragment>
+    );
   }
 }
