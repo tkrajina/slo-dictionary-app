e2eac2afb4b7798d9a74d872fb63a783cf7d0c37
Tomo Krajina <tkrajina@gmail.com>
2020-12-06 10:01:23 +0100

    Random screen working

--- src/screens/RandomScreen.tsx
+++ src/screens/RandomScreen.tsx
@@ -1,26 +1,23 @@
 import { default as React } from "react";
-import { Button, View } from "react-native";
+import { ActivityIndicator, Alert, Button, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
 import { AppScreenView } from "../components/AppScreenView";
 import { WordInfo } from "../components/WordInfo";
-import { ThesaurusEntry } from "../models/models";
+import { AbstractWord, CollocationEntry, ThesaurusEntry } from "../models/models";
 import { navigate } from "../navigation";
 import { Params, Routes, Stacks } from "../routes";
 import { stores } from "../stores/RootStore";
 import { CleanupContainer } from "../utils/cleanup";
+import { Model } from "../utils/dao";
 import * as Utils from "../utils/utils";
 import { ScreenProps } from "./common";
 
 class State {
-  word: ThesaurusEntry = new ThesaurusEntry();
+  word: AbstractWord | undefined;
 }
 
 // FIXME: Rename this
 export default class RandomScreen extends React.Component<ScreenProps, State> {
-  static navigationOptions = {
-    title: "Random",
-  };
-
   cleanup = new CleanupContainer();
 
   constructor(props: ScreenProps) {
@@ -36,19 +33,27 @@ export default class RandomScreen extends React.Component<ScreenProps, State> {
   }
 
   willFocus = async () => {
-    await this.callbackOnReload();
+    await this.callbackOnReload(ThesaurusEntry);
   };
 
   didBlur = () => {
     this.cleanup.cleanup();
   };
 
-  async callbackOnReload() {
-    const count = await stores.dao.countAsync(ThesaurusEntry, "");
-    const n = Math.ceil(Math.random() * count);
-    const words = await stores.dao.query(ThesaurusEntry, "where number=? limit 1", [n]);
+  async callbackOnReloadThesaurus() {
+    await this.callbackOnReload(ThesaurusEntry);
+  }
+
+  async callbackOnReloadCollocation() {
+    await this.callbackOnReload(CollocationEntry);
+  }
+
+  async callbackOnReload(cls: any) {
+    const count = await stores.dao.countAsync(cls as any, "");
+    const id = Math.ceil(Math.random() * count);
+    const words = await stores.dao.query(cls as any, "where id=? limit 1", [id]);
     this.setState({
-      word: words[0] || new ThesaurusEntry(),
+      word: (words[0] || new ThesaurusEntry()) as AbstractWord,
     });
   }
 
@@ -62,11 +67,13 @@ export default class RandomScreen extends React.Component<ScreenProps, State> {
         <View style={{ flexDirection: "column", flex: 1 }}>
           <View style={{ flex: 1 }}>
             <ScrollView>
-              <WordInfo word={this.state.word} long={true} onClickWord={this.callbackOnClick} />
+              {!this.state.word && <ActivityIndicator />}
+              {this.state.word && <WordInfo word={this.state.word} long={true} onClickWord={this.callbackOnClick} />}
             </ScrollView>
           </View>
-          <View style={{ height: 40 }}>
-            <Button title="Naslednja beseda" onPress={this.callbackOnReload} />
+          <View style={{ height: 80 }}>
+            <Button title="Naključna beseda (sopomenke)" onPress={this.callbackOnReloadThesaurus} />
+            <Button title="Naključna beseda (kolokacije)" onPress={this.callbackOnReloadCollocation} />
           </View>
         </View>
       </AppScreenView>
