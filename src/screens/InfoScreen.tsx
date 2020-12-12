a946befdc5b601d7d90b88c6722b879ef036bc22
Tomo Krajina <tkrajina@gmail.com>
2020-12-12 16:44:06 +0100

    Collocations in the info screen

--- src/screens/InfoScreen.tsx
+++ src/screens/InfoScreen.tsx
@@ -32,11 +32,18 @@ export default class InfoScreen extends React.Component<ScreenProps> {
 
 const MARKDOWN = `# Thesaurus of modern Slovene language
 
-This application is an unofficial offline viewer of the Thesaurus of modern Slovene language available on <https://viri.cjvt.si/sopomenke/>.
+This application is an unofficial offline viewer of the:
 
-The Thesaurus is published under Creative Commons Attribution-ShareAlike International 4.0. Authors: Simon Krek, Cyprian Laskowski, Marko Robnik Šikonja, Iztok Kosem, Špela Arhar Holdt, Polona Gantar, Jaka Čibej, Vojko Gorjanc, Bojan Klemenc, Kaja Dobrovoljc
+* Thesaurus of modern Slovene language <https://viri.cjvt.si/sopomenke/>
+* Collocations Dictionary of Modern Slovene <https://viri.cjvt.si/kolokacije/slv/>
 
-More information about the Thesaurus: <https://viri.cjvt.si/sopomenke/eng/about>
+The data is published under Creative Commons Attribution-ShareAlike International 4.0.
+
+Thesaurus Authors: Simon Krek, Cyprian Laskowski, Marko Robnik Šikonja, Iztok Kosem, Špela Arhar Holdt, Polona Gantar, Jaka Čibej, Vojko Gorjanc, Bojan Klemenc, Kaja Dobrovoljc
+
+Collocations authors: Iztok Kosem, Polona Gantar, Simon Krek, Špela Arhar Holdt, Jaka Čibej, Cyprian Laskowski, Eva Pori, Bojan Klemenc, Kaja Dobrovoljc, Vojko Gorjanc, Nikola Ljubešić
+
+Read more about on <https://viri.cjvt.si/sopomenke/eng/about> and <https://viri.cjvt.si/kolokacije/slv/about>.
 
 # Mobile app
 
